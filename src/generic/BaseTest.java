95758fc0be4198ab1b93af2d7ba8dc73560ef81d
ajitbiswas_Qspiders <ajit.biswas25@gmail.com>
2017-11-18 19:01:57 +0530

    Initial commit

--- src/generic/BaseTest.java
+++ src/generic/BaseTest.java
@@ -2,6 +2,7 @@ package generic;
 
 import java.util.concurrent.TimeUnit;
 
+import org.apache.logging.log4j.*;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
 import org.testng.annotations.AfterMethod;
@@ -11,6 +12,8 @@ public class BaseTest implements IAutoConstant{
 	
 	public WebDriver driver;
 	
+	Logger log = LogManager.getLogger(BaseTest.class.getName());
+	
 	static{
 		System.setProperty(GECKO_KEY, GECKO_VALUE);
 		System.setProperty(CHROME_KEY, CHROME_VALUE);
@@ -18,10 +21,12 @@ public class BaseTest implements IAutoConstant{
 	
 	@BeforeMethod
 	public void openApplication(){
-		
+		log.debug("launching the browser");
 		driver = new FirefoxDriver();
+		log.info("browser is lanuched");
 		String url = Lib.getProperty(CONFIG_PATH, "URL");
 		driver.get(url);
+		log.error("this is an error");
 		int timeout = Integer.parseInt(Lib.getProperty(CONFIG_PATH, "ITO"));
 		driver.manage().timeouts().implicitlyWait(timeout, TimeUnit.SECONDS);
 	}
@@ -30,6 +35,9 @@ public class BaseTest implements IAutoConstant{
 	public void closeApplication(){
 		
 		driver.close();
+		log.fatal("this is closing application - fatal");
 	}
+	
+	
 
 }
